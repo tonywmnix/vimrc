2e63a1d2e88de7d76380afd06f54f418f4b37713
Tony Moore <tonywmnix@gmail.com>
2014-07-15 06:40:35 -0700

    Add checkout script

---
+++ bundle/checkout_plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/vim-scripts/Align.git
+git clone https://github.com/wincent/Command-T.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/vim-scripts/DirDiff.vim.git
+git clone https://github.com/editorconfig/editorconfig-vim.git
+git clone https://github.com/vim-scripts/L9.git
+git clone https://github.com/tmhedberg/matchit.git
+git clone https://github.com/Lokaltog/powerline.git
+git clone https://github.com/kien/rainbow_parentheses.vim.git
+git clone https://github.com/kien/tabman.vim.git
+git clone https://github.com/vim-scripts/vcscommand.vim.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/thinca/vim-localrc.git
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/tpope/vim-sensible.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/vim-voom/VOoM.git
